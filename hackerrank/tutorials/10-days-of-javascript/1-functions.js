d9ff1d104316c483656ed2c32c5b46825bb18d76
faisal <faisalhmohd@gmail.com>
2019-08-19 10:04:32 +0000

    Day 1 - Functions

---
+++ hackerrank/tutorials/10-days-of-javascript/1-functions.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+    let result = 1;
+    n = parseInt(n);
+    for (let i = 1; i <= n; i++){
+        result = result * i;
+    }
+    return result;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
